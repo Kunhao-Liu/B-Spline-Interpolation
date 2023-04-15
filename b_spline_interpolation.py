55819cfb1807cea01682be26346f22d0fe4cfbd5
Kunhao-Liu <954001442@qq.com>
2023-04-15 17:49:31 +0800

    bspline interpolate

--- b_spline_interpolation.py
+++ b_spline_interpolation.py
@@ -1,117 +1,120 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 from basis import *
 
-degree = 3 # degree of the B-spline curve
 
-file_name = 'input_files/input.txt'
-with open(file_name, 'r') as f:
-    lines = f.readlines()
+def b_spline_interpolation(data_points, degree=3, plot=False, write_to_file=False):
+    num_data_points = len(data_points)
+    # compute the sum of the distances between the points
+    total_distance = 0
+    for i in range(1, len(data_points)):
+        total_distance += math.sqrt((data_points[i][0] - data_points[i-1][0])**2 + (data_points[i][1] - data_points[i-1][1])**2)
 
-data_points = [(float(line.split(' ')[0]), float(line.split(' ')[1])) for line in lines]
-num_data_points = len(data_points)
-print('read data points:', data_points)
+    # construct the knot vector using Chord Length Method
+    knot_vector = [0.]
+    for i in range(1, len(data_points)):
+        knot_vector.append(knot_vector[i-1] + math.sqrt((data_points[i][0] - data_points[i-1][0])**2 + (data_points[i][1] - data_points[i-1][1])**2)/total_distance)
 
-# compute the sum of the distances between the points
-total_distance = 0
-for i in range(1, len(data_points)):
-    total_distance += math.sqrt((data_points[i][0] - data_points[i-1][0])**2 + (data_points[i][1] - data_points[i-1][1])**2)
+    # append three 0 in the start and two 1 in the end of the knot vector
+    for i in range(degree):
+        knot_vector.insert(0, 0.)
+        knot_vector.append(1.)
 
-# construct the knot vector using Chord Length Method
-knot_vector = [0.]
-for i in range(1, len(data_points)):
-    knot_vector.append(knot_vector[i-1] + math.sqrt((data_points[i][0] - data_points[i-1][0])**2 + (data_points[i][1] - data_points[i-1][1])**2)/total_distance)
+    A = np.zeros((num_data_points, num_data_points+2))
+    for i in range(num_data_points):
+        for j in range(num_data_points+2):
+            A[i][j] = B(knot_vector[i+degree], degree, j, knot_vector)
+            
+    # insert the boundary conditions
+    con1 = np.zeros(num_data_points+2)
+    con2 = np.zeros(num_data_points+2)
+    for i in range(num_data_points+2):
+        con1[i] = B(knot_vector[degree], degree, i, knot_vector, derivative_order=2)
+        con2[i] = B(knot_vector[-1-degree], degree, i, knot_vector, derivative_order=2)
 
-# append three 0 in the start and two 1 in the end of the knot vector
-for i in range(degree):
-    knot_vector.insert(0, 0.)
-    knot_vector.append(1.)
+    # insert con1 to the scend row of A
+    A = np.insert(A, 1, con1, axis=0)
+    # insert con2 to the scend to last row of A
+    A = np.insert(A, -1, con2, axis=0)
+    det = np.linalg.det(A)
 
-print('knot vector:', knot_vector)
+    # convert x-coordinates and y-coordinates to two numpy vectors
+    data_x = np.array([p[0] for p in data_points])
+    data_y = np.array([p[1] for p in data_points])
+    # insert 0 in the start and end of x and y
+    data_x = np.insert(data_x, 1, 0)
+    data_x = np.insert(data_x, -1, 0)
+    data_y = np.insert(data_y, 1, 0)
+    data_y = np.insert(data_y, -1, 0)
 
+    # solve the linear system
+    control_x = np.linalg.solve(A, data_x)
+    control_y = np.linalg.solve(A, data_y)
 
-A = np.zeros((num_data_points, num_data_points+2))
-for i in range(num_data_points):
-    for j in range(num_data_points+2):
-        A[i][j] = B(knot_vector[i+degree], degree, j, knot_vector)
-        
-# insert the boundary conditions
-con1 = np.zeros(num_data_points+2)
-con2 = np.zeros(num_data_points+2)
-for i in range(num_data_points+2):
-    con1[i] = B(knot_vector[degree], degree, i, knot_vector, derivative_order=2)
-    con2[i] = B(knot_vector[-1-degree], degree, i, knot_vector, derivative_order=2)
+    if plot:
+        # visualize the b-spline curve
+        u = np.linspace(knot_vector[degree], knot_vector[-degree-1], 200)
+        x = [0. for i in range(len(u))]
+        y = [0. for i in range(len(u))]
+        for i in range(len(u)):
+            for j in range(len(control_x)):
+                x[i] += control_x[j] * B(u[i], degree, j, knot_vector)
+                y[i] += control_y[j] * B(u[i], degree, j, knot_vector)
+            
+        plt.xlabel('x')
+        plt.ylabel('y')
 
-# insert con1 to the scend row of A
-A = np.insert(A, 1, con1, axis=0)
-# insert con2 to the scend to last row of A
-A = np.insert(A, -1, con2, axis=0)
-det = np.linalg.det(A)
-# Print the result
-print(det)
-print(A)
-
-# convert x-coordinates and y-coordinates to two numpy vectors
-data_x = np.array([p[0] for p in data_points])
-data_y = np.array([p[1] for p in data_points])
-# insert 0 in the start and end of x and y
-data_x = np.insert(data_x, 1, 0)
-data_x = np.insert(data_x, -1, 0)
-data_y = np.insert(data_y, 1, 0)
-data_y = np.insert(data_y, -1, 0)
-
-# solve the linear system
-control_x = np.linalg.solve(A, data_x)
-control_y = np.linalg.solve(A, data_y)
-
-print('control points', [(control_x[i], control_y[i]) for i in range(len(control_x))] )
-
-# visualize the b-spline curve
-u = np.linspace(knot_vector[degree], knot_vector[-degree-1], 100)
-x = [0. for i in range(len(u))]
-y = [0. for i in range(len(u))]
-for i in range(len(u)):
-    for j in range(len(control_x)):
-        x[i] += control_x[j] * B(u[i], degree, j, knot_vector)
-        y[i] += control_y[j] * B(u[i], degree, j, knot_vector)
-        
-import matplotlib.pyplot as plt
-plt.xlabel('x')
-plt.ylabel('y')
-
-plt.plot(x, y, 'r', label='B-spline curve')
-# add labels
+        plt.plot(x, y, 'r', label='B-spline curve')
+        # add labels
 
 
-# plot the data points
-for i in range(len(data_points)):
-    plt.plot(data_points[i][0], data_points[i][1], 'bo', label='data points')
+        # plot the data points
+        for i in range(len(data_points)):
+            plt.plot(data_points[i][0], data_points[i][1], 'bo', label='data points')
 
-# plot the control points
-for i in range(len(control_x)):
-    plt.plot(control_x[i], control_y[i], 'gx' ,label='control points')
-# connect the control points
-for i in range(len(control_x)-1):
-    plt.plot([control_x[i], control_x[i+1]], [control_y[i], control_y[i+1]], 'g--')
+        # plot the control points
+        for i in range(len(control_x)):
+            plt.plot(control_x[i], control_y[i], 'gx' ,label='control points')
+        # connect the control points
+        for i in range(len(control_x)-1):
+            plt.plot([control_x[i], control_x[i+1]], [control_y[i], control_y[i+1]], 'g--')
 
-plt.axis('equal')
-handles, labels = plt.gca().get_legend_handles_labels()
-by_label = dict(zip(labels, handles))
-plt.legend(by_label.values(), by_label.keys())
-plt.show()
+        plt.axis('equal')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+        plt.show()
 
-# write the b-spline curve to a file
-with open('curve_'+file_name, 'w') as f:
-    # write the degree in a line
-    f.write(str(degree)+'\n')
-    # write the number of control points in a line
-    f.write(str(len(control_x))+'\n\n')
+    if write_to_file:
+        # write the b-spline curve to a file
+        with open('curve_'+file_name, 'w') as f:
+            # write the degree in a line
+            f.write(str(degree)+'\n')
+            # write the number of control points in a line
+            f.write(str(len(control_x))+'\n\n')
 
-    # write the knot vector in a line, each value rounded to 4 decimal places
-    for i in range(len(knot_vector)):
-        f.write(str(round(knot_vector[i], 3))+' ')
-    f.write('\n\n')
+            # write the knot vector in a line, each value rounded to 4 decimal places
+            for i in range(len(knot_vector)):
+                f.write(str(round(knot_vector[i], 3))+' ')
+            f.write('\n\n')
 
-    # write the control points, each point in a line
-    for i in range(len(control_x)):
-        f.write(str(round(control_x[i], 3))+' '+str(round(control_y[i], 3))+'\n')
+            # write the control points, each point in a line
+            for i in range(len(control_x)):
+                f.write(str(round(control_x[i], 3))+' '+str(round(control_y[i], 3))+'\n')
+
+    return control_x, control_y, knot_vector
+
+
+if __name__ == '__main__':
+    degree = 3 # degree of the B-spline curve
+
+    file_name = 'input_files/input.txt'
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+
+    data_points = [(float(line.split(' ')[0]), float(line.split(' ')[1])) for line in lines]
+    print('read data points:', data_points)
+
+
+    b_spline_interpolation(3, data_points, plot=True)
--- plot_curve.py
+++ plot_curve.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from b_spline_interpolation import *
 
 h = 25
 
@@ -11,7 +12,8 @@ def y_u(u):
 
 # tasks
 plot_curve = True
-cubic_polynomial = True
+cubic_polynomial = False
+b_spline = True
 
 if plot_curve:
     # plot the curve
@@ -29,9 +31,15 @@ if plot_curve:
 
 if cubic_polynomial:
     # sample points from the curve
-    u = np.linspace(0, 1, 10, endpoint=True)
-    x = np.array([[x_u(u_i)] for u_i in u]) # [10, 1]
-    y = np.array([[y_u(u_i)] for u_i in u]) # [10, 1]
+    num_points = 20
+    # u = np.linspace(0, 1, num_points, endpoint=True)
+    # u = np.random.uniform(low=0, high=1, size=num_points)
+    u = np.random.normal(0, 1, num_points)
+
+    u.sort()
+
+    x = np.array([[x_u(u_i)] for u_i in u]) 
+    y = np.array([[y_u(u_i)] for u_i in u]) 
 
     A = np.array([[1, u_i, u_i**2, u_i**3] for u_i in u]) # [10, c]
 
@@ -46,6 +54,14 @@ if cubic_polynomial:
     def cubic_polynomial_y(u):
         return coeff_y[0] + coeff_y[1]*u + coeff_y[2]*u**2 + coeff_y[3]*u**3
     
+    # calculate the error
+    error = 0
+    for u_i in u:
+        error += (cubic_polynomial_x(u_i) - x_u(u_i))**2 + (cubic_polynomial_y(u_i) - y_u(u_i))**2
+    error /= num_points
+    error = np.sqrt(error)
+    print(f'RMSE: {error.round(4)}')
+
     # plot the sampled points
     plt.plot(x, y, 'gx', label='sampled points')
 
@@ -53,6 +69,9 @@ if cubic_polynomial:
     u = np.linspace(0, 1, 100, endpoint=True)
     x = [cubic_polynomial_x(u_i) for u_i in u]
     y = [cubic_polynomial_y(u_i) for u_i in u]
+
+    plt.title(f'Randomly Sample {num_points} Points from a Normal Distribution')
+    # plt.title(f'Unifomly Sample {num_points} Points')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.plot(x, y, 'r', label='cubic polynomial curve')
@@ -61,4 +80,45 @@ if cubic_polynomial:
     by_label = dict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys())
     plt.show()
+
+if b_spline:
+    degree = 3
+
+    # sample points from the curve
+    num_points = 20
+    # u = np.linspace(0, 1, num_points, endpoint=True)
+    u = np.random.uniform(low=0, high=1, size=num_points)
+    # u = np.random.normal(0, 1, num_points)
+
+    u.sort()
+
+    x = np.array([[x_u(u_i)] for u_i in u]) 
+    y = np.array([[y_u(u_i)] for u_i in u]) 
+    data_points = np.concatenate((x, y), axis=1)
+    plt.plot(x, y, 'gx', label='sampled points')
+
+    # get the b-spline curve
+    control_x, control_y, knot_vector = b_spline_interpolation(data_points)
+
+
+    # visualize the b-spline curve
+    u = np.linspace(knot_vector[degree], knot_vector[-degree-1], 200)
+    x = [0. for i in range(len(u))]
+    y = [0. for i in range(len(u))]
+    for i in range(len(u)):
+        for j in range(len(control_x)):
+            x[i] += control_x[j] * B(u[i], degree, j, knot_vector)
+            y[i] += control_y[j] * B(u[i], degree, j, knot_vector)
+
+    plt.title(f'Randomly Sample {num_points} Points from a Uniform Distribution')
+    # plt.title(f'Unifomly Sample {num_points} Points')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(x, y, 'r', label='cubic B-spline curve')
+    plt.axis('equal')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
+    plt.show()
+
 
